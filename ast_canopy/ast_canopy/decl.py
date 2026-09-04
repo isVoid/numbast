e07d3cbe6daa10a444dfa29a4afe7e9e6f33ec5f
Michael Wang <wangm@ipp1-2890.ipp1a1.colossus.nvidia.com>
2026-09-04 20:38:26 +0000

    feat: add CUDA-Oxide C device binding backend

--- ast_canopy/ast_canopy/decl.py
+++ ast_canopy/ast_canopy/decl.py
@@ -6,8 +6,7 @@ import typing
 from functools import cached_property
 
 from ast_canopy import pylibastcanopy as bindings
-
-from ast_canopy.instantiations import FunctionInstantiation, ClassInstantiation
+from ast_canopy.instantiations import ClassInstantiation, FunctionInstantiation
 
 CXX_OP_TO_PYTHON_OP = {
     "+": [operator.pos, operator.add],
@@ -77,6 +76,8 @@ class Function:
         mangled_name: str,
         attributes: str,
         parse_entry_point: str,
+        is_c_linkage: bool | None = None,
+        is_variadic: bool = False,
     ):
         self.name = name
         self.qual_name = qual_name
@@ -88,6 +89,8 @@ class Function:
         self.is_constexpr = is_constexpr
         self.mangled_name = mangled_name
         self.attributes = attributes
+        self.is_c_linkage = is_c_linkage
+        self.is_variadic = is_variadic
 
         self.parse_entry_point = parse_entry_point
 
@@ -169,6 +172,8 @@ class Function:
             c_obj.mangled_name,
             c_obj.attributes,
             parse_entry_point,
+            getattr(c_obj, "is_c_linkage", None),
+            getattr(c_obj, "is_variadic", False),
         )
 
 
